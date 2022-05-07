e179cf6a033eb52816d6f03b672ff0b148596f32
Marsel Ishmukhametov <yamarvin01@yandex.ru>
2022-05-07 16:50:54 +0300

    Исправить замечания после Ревью

--- scripts/script.js
+++ scripts/script.js
@@ -8,7 +8,7 @@ const btnClosePopupEditProfile = popupEditProfile.querySelector('.popup__button_
 const formEditProfile = popupEditProfile.querySelector('.popup__form_type_edit-profile');
 const nameInput = formEditProfile.querySelector('.popup__input_type_name');
 const jobInput = formEditProfile.querySelector('.popup__input_type_text');
-const cardForm = document.querySelector('.popup__form_type_add-card');
+const formCard = document.querySelector('.popup__form_type_add-card');
 const popupAddCard = document.querySelector('.popup_type_add-card');
 const btnAddCard = profile.querySelector(".profile__button-add");
 const btnClosePopupAddCard = popupAddCard.querySelector('.popup__button_type_close');
@@ -51,7 +51,7 @@ function formSubmitHandler(event) {
   event.preventDefault();
   titleProfile.textContent = nameInput.value;
   subtitleProfile.textContent = jobInput.value;
-  closePopupEditProfile();
+  closePopup(event);
 }
 
 // Функция добавления на страницу и отправки на сервер новой карточки
@@ -61,8 +61,8 @@ function submitNewCard(event) {
   const cardName = event.target.querySelector('.popup__input_type_place').value;
   const newCard = {name: cardName, link: cardLink};
   addCardHtml(newCard);
-  cardForm.reset();
-  closePopupAddCard();
+  formCard.reset();
+  closePopup(event);
 }
 
 // Функция удаления карточки
@@ -82,7 +82,6 @@ function setActiveLikeBtn(event) {
 
 // Фнкция вывода изображения на полный экран
 function showImg(card) {
-  openPopupImg();
   const cardImage = card.querySelector('.card__image').src;
   const cardTitle = card.querySelector('.card__title').textContent;
   const popupImage = popupImg.querySelector('.popup__image');
@@ -91,20 +90,16 @@ function showImg(card) {
   popupTitle.textContent = cardTitle;
   popupImage.alt = '';
   popupImage.alt = 'Иллюстрация природы: ' + cardTitle;
+  openPopupImg();
 }
 
 function openEditForm(event) {
-  console.log(event);
-  openPopupEditProfile();
   nameInput.value = titleProfile.textContent;
   jobInput.value = subtitleProfile.textContent;
+  openPopupEditProfile();
 }
 
 function openPopupEditProfile() {
-  toggleModal(popupEditProfile);
-}
-
-function closePopupEditProfile() {
   toggleModal(popupEditProfile);
 }
 
@@ -112,27 +107,23 @@ function openPopupAddCard() {
   toggleModal(popupAddCard);
 }
 
-function closePopupAddCard() {
-  toggleModal(popupAddCard);
-}
-
 function openPopupImg() {
   toggleModal(popupImg);
 }
 
-function closePopupImg() {
-  toggleModal(popupImg);
+function closePopup(event) {
+  const popup = event.currentTarget.closest('.popup');
+  popup.classList.remove('popup_opened');
 }
 
-
-
-btnEditProfile.addEventListener('click', openEditForm);
-btnClosePopupEditProfile.addEventListener('click', closePopupEditProfile);
 formEditProfile.addEventListener('submit', formSubmitHandler);
+formCard.addEventListener('submit', submitNewCard);
+btnEditProfile.addEventListener('click', openEditForm);
 btnAddCard.addEventListener('click', openPopupAddCard);
-btnClosePopupAddCard.addEventListener('click', closePopupAddCard);
-cardForm.addEventListener('submit', submitNewCard);
-btnClosePopupImg.addEventListener('click', closePopupImg);
+
+btnClosePopupAddCard.addEventListener('click', closePopup);
+btnClosePopupEditProfile.addEventListener('click', closePopup);
+btnClosePopupImg.addEventListener('click', closePopup);
 
 
 
